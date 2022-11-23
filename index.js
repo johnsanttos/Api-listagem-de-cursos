29b4c04d5918f6ff69d715b54655dbee6fb5c789
John Santos <84046012+johnsanttos@users.noreply.github.com>
2022-11-23 14:23:47 -0300

    Finalizando middlewares

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express')
 
-
 const server = express()
 
 server.use(express.json())
 
+const cursos = ['NodeJs', 'javascript', 'React Native']
 
-const cursos = [ 'NodeJs' , 'javascript', 'React Native']
+//Middleware Global
+server.use((req, res, next) => {
+  console.log(`URL CHAMADA ${req.url}`)
+  return next()
+})
 
-server.get("/cursos", (req,res)=>{
-	return res.json(cursos)
+function checkCurso(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'Nome do curso é obrigatorio!' })
+  }
+  return next()
+}
+
+function checkIndexCurso(req,res, next){
+const curso = cursos[req.params.index]
+if(!curso){
+	return res.status(400).json({error: "O curso não existe"})
+}
+
+req.curso = curso
+return next()
+}
+
+server.get('/cursos', (req, res) => {
+  return res.json(cursos)
 })
 
 // Query params = ?nome=NodeJS
@@ -17,27 +38,37 @@ server.get("/cursos", (req,res)=>{
 // Request Body = {nome: 'NodeJs', tipo: 'Backend'  }
 
 //localhost:3000/curso
-server.get('/cursos/:index', (req,res) => {
+server.get('/cursos/:index',checkIndexCurso, (req, res) => {
+  const { index } = req.params
 
-	const {index} = req.params
-	
-	return res.json(cursos[index])
+  return res.json(req.curso)
 })
 
 //criando um novo curso
-server.post('/cursos',(req, res) =>{
-	const {name} = req.body;
-	cursos.push(name)
+// .push adiciona item ao array
+server.post('/cursos', checkCurso, (req, res) => {
+  const { name } = req.body
+  cursos.push(name)
 
-	return res.json(cursos)
+  return res.json(cursos)
 })
 
 //atualizando um curso
 
-server.put('/cursos/:index',(req,res)=>{
-	const {index} =req.params;
-	const{name} =req.body
-	cursos[index] = name
-	return res.json(cursos)
+server.put('/cursos/:index', checkCurso, checkIndexCurso, (req, res) => {
+  const { index } = req.params
+  const { name } = req.body
+  cursos[index] = name
+  return res.json(cursos)
+})
+
+//excluindo curso
+// splice do javascript deleta do array
+server.delete('/cursos/:index', (req, res) => {
+  const { index } = req.params
+
+  cursos.splice(index, 1)
+  //return res.json(cursos)
+  return res.json({ message: 'Curso deletado com sucesso!' })
 })
 server.listen(3000)
